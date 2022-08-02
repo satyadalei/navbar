c28ec88025f466fa03c93115506f21eba21c2ce3
satyanarayan453 <74782403+satyanarayan453@users.noreply.github.com>
2022-08-02 20:26:04 +0530

    responsive navbar created only

---
+++ index.js
@@ -0,0 +1,13 @@
+let bars = document.getElementById("3bar") ;
+let navBox = document.getElementById("nav-item-box");
+let allNavLinks = document.querySelectorAll(".nav-links");
+ 
+function toogleActiveClass(){
+    bars.classList.toggle("active");
+    navBox.classList.toggle("menu-active");
+}
+
+bars.addEventListener("click", toogleActiveClass);
+allNavLinks.forEach(function(navItem){
+    navItem.addEventListener("click",toogleActiveClass);
+})
